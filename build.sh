be12cbcd6ae595854641e34d63ccd4ce62d7ffa3
Arthur Naseef <art@artnaseef.com>
2021-12-01 17:14:39 -0700

    Correct the Karaf version in the build script

--- build.sh
+++ build.sh
@@ -17,7 +17,7 @@
 ################################################################################
 
 DOCKER_IMAGE_NAME=artnaseef/karaf
-DOCKER_IMAGE_VERSION=4.3.1
+DOCKER_IMAGE_VERSION=4.3.3
 
 docker rmi --force=true ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_VERSION}
 docker build --force-rm=true --rm=true -t ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_VERSION} .
